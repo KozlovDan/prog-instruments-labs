b54a505c3e23146e3a125ccd9f7d0f9a9497ef3d
KozlovDan <dankoz2005@gmail.com>
2025-12-18 01:16:13 +0400

    Delete lab_6 directory

--- lab_6/transaction.py
+++
@@ -1,6 +0,0 @@
-class Transaction:
-    def __init__(self, description: str, amount: float):
-        if not isinstance(amount, (int, float)):
-            raise TypeError("Amount must be a number")
-        self.description = description
-        self.amount = amount
